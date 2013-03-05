1d4586b246f5417572cc908ee3733d182e1859ab
arthurkam <arthur.kam@hotmail.com>
2013-03-05 11:16:03 -0500

    Add and fixed some navigator classes
    Add in travelTo(x,y,theta) for Navigator.java
    Fixed the constructor for ObstacleNavigator by adding in the sensor
    ports for it

--- code/src/ca/mcgill/dpm/winter2013/group6/navigator/Navigator.java
+++ code/src/ca/mcgill/dpm/winter2013/group6/navigator/Navigator.java
@@ -4,14 +4,14 @@ package ca.mcgill.dpm.winter2013.group6.navigator;
  * Navigator interface which defines a set of methods that any robot that can
  * navigate should adhere to. Specifically, going to a set of coordinates and
  * turning.
- *
+ * 
  * @author Alex Selesse
- *
+ * 
  */
 public interface Navigator extends Runnable {
   /**
    * Moves to the robot to (x, y) coordinates.
-   *
+   * 
    * @param x
    *          The x-coordinate we're going to.
    * @param y
@@ -20,16 +20,30 @@ public interface Navigator extends Runnable {
   void travelTo(double x, double y);
 
   /**
+   * Moves to the robot to (x, y, omega) coordinates.
+   * 
+   * @param x
+   *          The x-coordinate we're going to.
+   * @param y
+   *          The y-coordinate we're going to.
+   * 
+   * @param theta
+   *          The angle the robot will travelling to.
+   */
+  void travelTo(double x, double y, double theta);
+
+  /**
    * Turn the robot by "theta" degrees.
-   *
+   * 
    * @param theta
    *          The degrees we want to turn to.
    */
+
   void turnTo(double theta);
 
   /**
    * Check whether the robot is currently traveling or rotating
-   *
+   * 
    * @return True if the robot is traveling or rotating, false otherwise.
    */
   boolean isNavigating();
--- code/src/ca/mcgill/dpm/winter2013/group6/navigator/ObstacleNavigator.java
+++ code/src/ca/mcgill/dpm/winter2013/group6/navigator/ObstacleNavigator.java
@@ -1,19 +1,28 @@
 package ca.mcgill.dpm.winter2013.group6.navigator;
 
 import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
 import ca.mcgill.dpm.winter2013.group6.odometer.Odometer;
 
 /**
  * {@link Navigator} implementation which takes into consideration obstacles.
- *
+ * 
  * @author Alex Selesse
- *
+ * 
  */
 public class ObstacleNavigator extends NoObstacleNavigator {
+  protected UltrasonicSensor uSensor;
+  protected TouchSensor leftSensor;
+  protected TouchSensor rightSensor;
 
   public ObstacleNavigator(Odometer odometer, NXTRegulatedMotor leftMotor,
-      NXTRegulatedMotor rightMotor) {
+      NXTRegulatedMotor rightMotor, UltrasonicSensor uSensor, TouchSensor leftSensor,
+      TouchSensor rightSensor) {
     super(odometer, leftMotor, rightMotor);
+    this.uSensor = uSensor;
+    this.leftSensor = leftSensor;
+    this.rightSensor = rightSensor;
   }
 
   @Override
