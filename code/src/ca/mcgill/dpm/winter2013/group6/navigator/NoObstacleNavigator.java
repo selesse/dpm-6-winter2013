a4b96b034aaacb390ba9e01f5ce099458f0e84cf
arthurkam <arthur.kam@hotmail.com>
2013-03-05 11:21:37 -0500

    Implemented the basic travelTo function
    implemented the travelTo function for NoObstacleNaviator

--- code/src/ca/mcgill/dpm/winter2013/group6/navigator/NoObstacleNavigator.java
+++ code/src/ca/mcgill/dpm/winter2013/group6/navigator/NoObstacleNavigator.java
@@ -1,16 +1,18 @@
 package ca.mcgill.dpm.winter2013.group6.navigator;
 
 import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.Sound;
 import ca.mcgill.dpm.winter2013.group6.odometer.Odometer;
 
 /**
  * {@link Navigator} implementation which does not take into consideration
  * obstacles.
- *
+ * 
  * @author Alex Selesse
- *
+ * 
  */
 public class NoObstacleNavigator extends AbstractNavigator {
+  int FORWARD_SPEED = 500;
 
   public NoObstacleNavigator(Odometer odometer, NXTRegulatedMotor leftMotor,
       NXTRegulatedMotor rightMotor) {
@@ -19,13 +21,34 @@ public class NoObstacleNavigator extends AbstractNavigator {
 
   @Override
   public void travelTo(double x, double y) {
-    // TODO
+
+    double xToll = 1, yToll = 1;
+    // runs till it is within the tolerance
+    while (Math.abs(x - odometer.getX()) > xToll && Math.abs(y - odometer.getY()) > yToll) {
+
+      // Calculate the new destinaton theta to turn
+      double desTheta = Math.atan2(x - odometer.getX(), y - odometer.getY());
+      desTheta = Math.toDegrees(desTheta);
+      // not sure about the correctness of this part
+      turnTo(desTheta);
+      setSpeed(FORWARD_SPEED, FORWARD_SPEED);
+    }
+    Sound.beep();
+    stop();
 
   }
 
   @Override
   public void run() {
     // TODO
+
+  }
+
+  @Override
+  public void travelTo(double x, double y, double theta) {
+    // testing needed!
+    travelTo(x, y);
+    turnTo(theta);
 
   }
 
