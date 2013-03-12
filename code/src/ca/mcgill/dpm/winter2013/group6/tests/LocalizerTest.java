443abef2159ffe30e1903fd5f6f0308210903dce
arthurkam <arthur.kam@hotmail.com>
2013-03-12 15:19:19 -0400

    Finish up writing the test for localization

--- code/src/ca/mcgill/dpm/winter2013/group6/tests/LocalizerTest.java
+++ code/src/ca/mcgill/dpm/winter2013/group6/tests/LocalizerTest.java
@@ -1,13 +1,23 @@
 package ca.mcgill.dpm.winter2013.group6.tests;
 
+import lejos.nxt.LightSensor;
+import lejos.nxt.UltrasonicSensor;
 import ca.mcgill.dpm.winter2013.group6.localization.AbstractLocalizer;
+import ca.mcgill.dpm.winter2013.group6.localization.LightLocalizer;
+import ca.mcgill.dpm.winter2013.group6.localization.Localizer;
+import ca.mcgill.dpm.winter2013.group6.localization.USLocalizer;
 import ca.mcgill.dpm.winter2013.group6.navigator.Navigator;
 import ca.mcgill.dpm.winter2013.group6.odometer.Odometer;
 
 public class LocalizerTest extends AbstractLocalizer {
+  UltrasonicSensor us;
+  LightSensor ls;
 
-  public LocalizerTest(Odometer odometer, Navigator navigator) {
-    super(odometer, navigator);
+  public LocalizerTest(Odometer odometer, Navigator navigator, UltrasonicSensor us, LightSensor ls,
+      int corner) {
+    super(odometer, navigator, corner);
+    this.us = us;
+    this.ls = ls;
   }
 
   @Override
@@ -16,14 +26,22 @@ public class LocalizerTest extends AbstractLocalizer {
   }
 
   public void lightLocalizerTest() {
+    Localizer tester = new LightLocalizer(odometer, navigator, corner, ls);
+    tester.doLocalize();
   }
 
   public void USLocalizerTest() {
+    Localizer tester = new USLocalizer(odometer, us, corner, navigator);
+    tester.doLocalize();
   }
 
   @Override
   public void doLocalize() {
-    // TODO Auto-generated method stub
+    USLocalizerTest();
+    lightLocalizerTest();
+    int destX = (corner == 2 || corner == 3) ? (int) 30.5 * 10 : 0;
+    int destY = (corner == 3 || corner == 4) ? (int) 30.5 * 10 : 0;
+    navigator.travelTo(destX, destY);
 
   }
 }
