8f1bb98e5705ea396f63765aa7459e9e0665e030
arthurkam <arthur.kam@hotmail.com>
2013-03-12 14:49:02 -0400

    Add in javadoc for AbstractLocalizer.java

--- code/src/ca/mcgill/dpm/winter2013/group6/localization/AbstractLocalizer.java
+++ code/src/ca/mcgill/dpm/winter2013/group6/localization/AbstractLocalizer.java
@@ -4,12 +4,29 @@ import ca.mcgill.dpm.winter2013.group6.navigator.Navigator;
 import ca.mcgill.dpm.winter2013.group6.odometer.Odometer;
 import ca.mcgill.dpm.winter2013.group6.util.Robot;
 
+/**
+ * The AbstractLocalizer provides a base contructor for other Localizer classes
+ * to use
+ * 
+ * @author arthurkam
+ */
 public abstract class AbstractLocalizer implements Localizer {
+
   protected Odometer odometer;
   protected Navigator navigator;
   protected Robot robot;
   protected int corner;
 
+  /**
+   * Initializes the Localizer using the given parameters
+   * 
+   * @param odometer
+   *          The odometer is used to get the angle and make adjustments to it
+   * @param navigator
+   *          The navigator is used to control the robot
+   * @param corner
+   *          A corner number to indicated the starting corner
+   */
   public AbstractLocalizer(Odometer odometer, Navigator navigator, int corner) {
     this.odometer = odometer;
     this.navigator = navigator;
