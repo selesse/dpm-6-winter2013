216ed13e0ccb4a6b5f699ae44f3662cad649cde3
Alex Selesse <alex@selesse.com>
2013-03-19 21:18:09 -0400

    Add a few dummy classes for tests.

---
+++ code/src/ca/mcgill/dpm/winter2013/group6/tests/bluetooth/BluetoothTest131.java
@@ -0,0 +1,7 @@
+package ca.mcgill.dpm.winter2013.group6.tests.bluetooth;
+
+import ca.mcgill.dpm.winter2013.group6.bluetooth.BluetoothThread;
+
+public class BluetoothTest131 extends BluetoothThread {
+
+}
---
+++ code/src/ca/mcgill/dpm/winter2013/group6/tests/launcher/BallLauncherTest141.java
@@ -0,0 +1,12 @@
+package ca.mcgill.dpm.winter2013.group6.tests.launcher;
+
+import lejos.nxt.NXTRegulatedMotor;
+import ca.mcgill.dpm.winter2013.group6.launcher.BallLauncherImpl;
+
+public class BallLauncherTest141 extends BallLauncherImpl {
+
+  public BallLauncherTest141(NXTRegulatedMotor ballThrowingMotor, double distanceFromTarget) {
+    super(ballThrowingMotor, distanceFromTarget);
+  }
+
+}
