4f87c3c34583562d78c450eb96fd29c3668c438a
arthurkam <arthur.kam@hotmail.com>
2013-03-23 00:16:56 -0400

    Start to implement odometery Correction

--- code/src/ca/mcgill/dpm/winter2013/group6/odometer/OdometerCorrection.java
+++ code/src/ca/mcgill/dpm/winter2013/group6/odometer/OdometerCorrection.java
@@ -2,8 +2,9 @@ package ca.mcgill.dpm.winter2013.group6.odometer;
 
 import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
+import lejos.util.TimerListener;
 
-public class OdometerCorrection implements Runnable {
+public class OdometerCorrection implements TimerListener {
   private Odometer odometer;
   private LightSensor ls;
   private static int threshold = 50;
@@ -17,21 +18,51 @@ public class OdometerCorrection implements Runnable {
   }
 
   @Override
-  public void run() {
-    // TODO Auto-generated method stub
+  public void timedOut() {
+    // if they have the same speed == moving straight, going forward
+    if (Motor.A.getRotationSpeed() == Motor.B.getRotationSpeed() && closest90() > 0
+        && Motor.A.getRotationSpeed() > 0) {
+
+    }
 
   }
 
-  public static int axis() {
+  public boolean cloestAxis() {
+    double x = odometer.getX() % 30.48;
+    double y = odometer.getY() % 30.48;
+    double theta = odometer.getTheta();
+    double speed = Motor.A.getRotationSpeed();
+    double xComp = Math.cos(theta);
+    double yComp = Math.sin(theta);
 
   }
 
-  public int closestGridLine() {
+  public int getClosestGridLine() {
     double position[] = new double[3];
     this.odometer.getPosition(position, new boolean[] { true, true, true });
     if (Motor.A.getRotationSpeed() != Motor.B.getRotationSpeed()) {
       return -1;
     }
 
+  }
+
+  public int closest90() {
+    double range = 5;
+    double theta = odometer.getTheta();
+    if (0 + range > theta && 0 - range < theta) {
+      return 1;
+    }
+    else if (90 + range > theta && 90 - range < theta) {
+      return 1;
+    }
+    else if (180 + range > theta && 180 - range < theta) {
+      return 1;
+    }
+    else if (270 + range > theta && 270 - range < theta) {
+      return 1;
+    }
+    else {
+      return -1;
+    }
   }
 }
