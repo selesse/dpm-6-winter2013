fc8f2ce8ac7340be8ccbd2c37f5f487bf94442ee
Alex Selesse <alex@selesse.com>
2013-03-24 13:04:12 -0400

    Switch some method names to be more descriptive.

--- code/src/ca/mcgill/dpm/winter2013/group6/bluetooth/BluetoothThread.java
+++ code/src/ca/mcgill/dpm/winter2013/group6/bluetooth/BluetoothThread.java
@@ -2,9 +2,9 @@ package ca.mcgill.dpm.winter2013.group6.bluetooth;
 
 /**
  * Bluetooth thread that hosts a {@link BluetoothConnection}.
- *
+ * 
  * @author Alex Selesse
- *
+ * 
  */
 public class BluetoothThread implements Runnable {
   @Override
--- code/src/ca/mcgill/dpm/winter2013/group6/odometer/OdometerCorrection.java
+++ code/src/ca/mcgill/dpm/winter2013/group6/odometer/OdometerCorrection.java
@@ -7,7 +7,7 @@ import lejos.util.TimerListener;
 
 public class OdometerCorrection implements TimerListener {
   private Odometer odometer;
-  private LightSensor ls;
+  private LightSensor lightSensor;
   private static int threshold = 50;
   private int sensorAverage = 0;
   private static final double LIGHT_SENSOR_DISTANCE = 11.6;
@@ -16,7 +16,7 @@ public class OdometerCorrection implements TimerListener {
 
   public OdometerCorrection(Odometer odometer, LightSensor lightSensor) {
     this.odometer = odometer;
-    this.ls = lightSensor;
+    this.lightSensor = lightSensor;
     this.timer = new Timer(PERIOD, this);
 
   }
@@ -24,16 +24,15 @@ public class OdometerCorrection implements TimerListener {
   @Override
   public void timedOut() {
     // if they have the same speed == moving straight, going forward
-    if (Motor.A.getRotationSpeed() == Motor.B.getRotationSpeed() && closest90() > 0
+    if (Motor.A.getRotationSpeed() == Motor.B.getRotationSpeed() && getQuadrantRobotIsFacing() > 0
         && Motor.A.getRotationSpeed() > 0) {
-      if (closestAxis()) {
+      if (isCloserToXAxis()) {
 
       }
       else {
 
       }
     }
-
   }
 
   /**
@@ -41,7 +40,7 @@ public class OdometerCorrection implements TimerListener {
    * 
    * @return true if x is closer, else y
    */
-  public boolean closestAxis() {
+  public boolean isCloserToXAxis() {
     double x = odometer.getX() % 30.48;
     double y = odometer.getY() % 30.48;
     if (x > 30.48 / 2) {
@@ -81,14 +80,13 @@ public class OdometerCorrection implements TimerListener {
       return mult * 30.48 + 30.48;
     }
     return mult * 30.48;
-
   }
 
   /**
    * 
    * @return the direction the robot is facing, returns a -1 if its not decisive
    */
-  public int closest90() {
+  public int getQuadrantRobotIsFacing() {
     double range = 5;
     double theta = odometer.getTheta();
     if (0 + range > theta && 0 - range < theta) {
@@ -110,12 +108,10 @@ public class OdometerCorrection implements TimerListener {
 
   public void start() {
     this.timer.start();
-
   }
 
   public void stop() {
     this.timer.stop();
-
   }
 
 }
